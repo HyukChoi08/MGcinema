20fa770a5b7d112dc5db1110c3d5ee2a9198a83b
1234 <1234@>
2024-08-20 16:46:25 +0900

    wt

---
+++ src/main/java/com/cinema/MGcinemaApplication.java
@@ -0,0 +1,13 @@
+package com.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MGcinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MGcinemaApplication.class, args);
+	}
+
+}
