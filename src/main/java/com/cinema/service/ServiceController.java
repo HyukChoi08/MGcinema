b9ae47f5fd6cc682a06133b1c95118871339944d
1234 <1234@>
2024-08-22 12:24:07 +0900

    아 제발 들어가라

---
+++ src/main/java/com/cinema/service/ServiceController.java
@@ -0,0 +1,17 @@
+package com.cinema.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ServiceController {
+
+    @GetMapping("/serviceHome")
+    public String getServiceHome(@RequestParam(value = "id", defaultValue = "1") int id, Model model) {
+        ServiceHomeDTO serviceHome = new ServiceHomeDTO("고객센터", "아직아직아직");
+        model.addAttribute("serviceHome", serviceHome);
+        return "service/ServiceHome";
+    }
+}
---
+++ src/main/java/com/cinema/service/ServiceHomeDTO.java
@@ -0,0 +1,13 @@
+package com.cinema.service;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServiceHomeDTO {
+    private String title;
+    private String content;
+}
