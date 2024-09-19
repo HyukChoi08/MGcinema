dbd062b82a4881f891c122758714953748eb9a02
bbiiyo1 <bbiiiyo.20bbiii.yo.2022@gmail.com>
2024-09-19 17:34:02 +0900

    00

--- src/main/java/com/cinema/home/logDAO.java
+++ src/main/java/com/cinema/home/logDAO.java
@@ -8,7 +8,8 @@ public interface logDAO {
 	
     void insertSignup(String realname, String email, String uid, 
     		String passwd, String birthday, String mobile,
-    		String region, String nickname, String favorite, String tellecom);	// 회원가입
+    		String region, String nickname, String favorite, String tellecom,
+    		String address, String post);	// 회원가입
     
     int checkUidExists(String uid); // 아이디 중복 체크
     
